a404885d5217ee034bafdc26bce4ca599c995518
it02 <it02@aoi-bj.cn>
2022-05-23 10:09:58 +0800

    settings ready

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -4,119 +4,26 @@
     <div class="column">
       Button Functions:
       <q-list bordered>
-        <q-item>
-          <q-item-section class="col-4 text-center"> H </q-item-section>
+        <q-item v-for="(m, index) in btnMode" :key="index">
+          <q-item-section class="col-4 text-center">
+            {{ m.mode }}
+          </q-item-section>
           <q-item-section class="col">
             <div class="text-center">
               <q-btn-toggle
-                v-model="mode.H"
+                v-model="m.status"
                 toggle-color="primary"
                 :options="[
                   { label: 'ON', value: true },
                   { label: 'OFF', value: false },
                 ]"
-                @update:model-value="setFunc(mode.H, 6)"
               />
+              <!-- @update:model-value="setFunc(m)" -->
             </div>
           </q-item-section>
         </q-item>
-        <q-item>
-          <q-item-section class="col-4 text-center"> M </q-item-section>
-          <q-item-section class="col">
-            <div class="text-center">
-              <q-btn-toggle
-                v-model="mode.M"
-                toggle-color="primary"
-                :options="[
-                  { label: 'ON', value: true },
-                  { label: 'OFF', value: false },
-                ]"
-                @update:model-value="setFunc(mode.M, 5)"
-              />
-            </div>
-          </q-item-section>
-        </q-item>
-        <q-item>
-          <q-item-section class="col-4 text-center"> L </q-item-section>
-          <q-item-section class="col">
-            <div class="text-center">
-              <q-btn-toggle
-                v-model="mode.L"
-                toggle-color="primary"
-                :options="[
-                  { label: 'ON', value: true },
-                  { label: 'OFF', value: false },
-                ]"
-                @update:model-value="setFunc(mode.L, 4)"
-              />
-            </div>
-          </q-item-section>
-        </q-item>
-        <q-item>
-          <q-item-section class="col-4 text-center"> C </q-item-section>
-          <q-item-section class="col">
-            <div class="text-center">
-              <q-btn-toggle
-                v-model="mode.C"
-                toggle-color="primary"
-                :options="[
-                  { label: 'ON', value: true },
-                  { label: 'OFF', value: false },
-                ]"
-                @update:model-value="setFunc(mode.C, 3)"
-              />
-            </div>
-          </q-item-section>
-        </q-item>
-        <q-item>
-          <q-item-section class="col-4 text-center"> F1 </q-item-section>
-          <q-item-section class="col">
-            <div class="text-center">
-              <q-btn-toggle
-                v-model="mode.F1"
-                toggle-color="primary"
-                :options="[
-                  { label: 'ON', value: true },
-                  { label: 'OFF', value: false },
-                ]"
-                @update:model-value="setFunc(mode.F1, 2)"
-              />
-            </div>
-          </q-item-section>
-        </q-item>
-        <q-item>
-          <q-item-section class="col-4 text-center"> F2 </q-item-section>
-          <q-item-section class="col">
-            <div class="text-center">
-              <q-btn-toggle
-                v-model="mode.F2"
-                toggle-color="primary"
-                :options="[
-                  { label: 'ON', value: true },
-                  { label: 'OFF', value: false },
-                ]"
-                @update:model-value="setFunc(mode.F2, 1)"
-              />
-            </div>
-          </q-item-section>
-        </q-item>
-        <q-item>
-          <q-item-section class="col-4 text-center"> SOS </q-item-section>
-          <q-item-section class="col">
-            <div class="text-center">
-              <q-btn-toggle
-                v-model="mode.SOS"
-                toggle-color="primary"
-                :options="[
-                  { label: 'ON', value: true },
-                  { label: 'OFF', value: false },
-                ]"
-                @update:model-value="setFunc(mode.SOS, 0)"
-              />
-            </div>
-          </q-item-section>
-        </q-item>
-        <q-item v-show="false">
+
+        <q-item v-show="true">
           <q-item-section class="col-5 q-my-xs q-mx-auto">
             <q-btn label="confirm" @click="confirm" />
           </q-item-section>
@@ -133,34 +40,68 @@ export default defineComponent({
   name: 'SettingsPage',
   setup() {
     //
-    const mode = ref({
-      H: true,
-      M: true,
-      L: true,
-      C: true,
-      F1: true,
-      F2: true,
-      SOS: true,
-    });
+    interface BtnMode {
+      mode: string;
+      index: number;
+      status: boolean;
+    }
+    const btnMode = ref(<BtnMode[]>[
+      {
+        mode: 'H',
+        index: 6,
+        status: true,
+      },
+      {
+        mode: 'M',
+        index: 5,
+        status: true,
+      },
+      {
+        mode: 'L',
+        index: 4,
+        status: true,
+      },
+      {
+        mode: 'C',
+        index: 3,
+        status: true,
+      },
+      {
+        mode: 'F1',
+        index: 2,
+        status: true,
+      },
+      {
+        mode: 'F2',
+        index: 1,
+        status: true,
+      },
+      {
+        mode: 'SOS',
+        index: 0,
+        status: true,
+      },
+    ]);
 
-    const setFunc = (val: boolean, index: number) => {
-      let num = 0;
-      if (val) {
-        num = 2 ** index;
-      }
-      console.log(num);
-    };
+    // const setFunc = (m: BtnMode) => {
+    //   const val = 2 ** m.index;
+    //   console.log(val)
+    // };
 
     const confirm = () => {
-      for (let item in mode.value) {
-        console.log(item);
-      }
+      let ret = 0;
+      btnMode.value.forEach((mode) => {
+        if (mode.status) {
+          ret += 2 ** mode.index;
+        }
+      });
+      console.log(ret);
     };
 
     return {
-      mode,
+      btnMode,
       confirm,
-      setFunc,
+      // setFunc,
     };
   },
 });
